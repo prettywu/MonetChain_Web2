e2619e89c07e196c561597a6df81faf08a105861
吴辉 <838622886@qq.com>
2018-07-24 21:46:23 +0800

    移动端兼容性调整

--- content/js/main.js
+++ content/js/main.js
@@ -12,26 +12,34 @@ var canvas2;
 var ctx2;
 
 $(function () {
-    $("#nav_right a").click(function () {
-        //"a.title-dw" title-dw是啊标签的css选择器class="title-dw".
-        $(".view-box").animate({
-            scrollTop: $($(this).attr("href")).get(0).offsetTop + -100 + "px"
-            //document.getElementById("section4").offsetTop
-        }, {
-                duration: 800,
-                easing: "swing"
-            });
-        return false;
-    });
-
-
+    // if(!browser.versions.mobile){
+    //     $("#nav_right a").click(function () {
+    //         $(".view-box").animate({
+    //             scrollTop: $($(this).attr("href")).get(0).offsetTop + -100 + "px"
+    //         }, {
+    //                 duration: 800,
+    //                 easing: "swing"
+    //             });
+    //         return false;
+    //     });
+    // }
 });
 
 $(window).on("load resize", function () {
     var wh = window.innerHeight || document.body.clientHeight || document.documentElement.clientHeight;
     $("#section1").css("height", wh);
-    $('.view-box').scrollspy({ target: '#nav_right', offset: 150 });
-    animation();
+    
+    
+    if (!browser.versions.mobile) {
+        alert('mobile');
+        $('.view-box').scrollspy({ target: '#nav_right', offset: 150 });
+        animation();
+    }
+    else{
+        alert($('.banner-left').parent().height());
+        $('.banner-left').height($('.banner-left').parent().height());
+    }
+
 
     Highcharts.chart('chart_box', {
         exporting: { enabled: false },
@@ -118,6 +126,13 @@ $(window).on("load resize", function () {
 function initAnimation() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
+
+    if (!ctx) {
+        alert('不支持html5');
+        return;
+    }
+
+
     // w = canvas.width = window.innerWidth,
     // h = canvas.height = window.innerHeight,
     w = canvas.width = document.getElementById('section1').clientWidth;
@@ -236,4 +251,27 @@ function getElementSize(id, type) {
         return arr[1];
     else
         return null;
+}
+
+//判断访问终端
+var browser = {
+    versions: function () {
+        var u = navigator.userAgent,
+            app = navigator.appVersion;
+        return {
+            trident: u.indexOf('Trident') > -1, //IE内核
+            presto: u.indexOf('Presto') > -1, //opera内核
+            webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
+            gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1,//火狐内核
+            mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
+            ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
+            android: u.indexOf('Android') > -1 || u.indexOf('Adr') > -1, //android终端
+            iPhone: u.indexOf('iPhone') > -1, //是否为iPhone或者QQHD浏览器
+            iPad: u.indexOf('iPad') > -1, //是否iPad
+            webApp: u.indexOf('Safari') == -1, //是否web应该程序，没有头部与底部
+            weixin: u.indexOf('MicroMessenger') > -1, //是否微信 （2015-01-22新增）
+            qq: u.match(/\sQQ/i) == " qq" //是否QQ
+        };
+    }(),
+    language: (navigator.browserLanguage || navigator.language).toLowerCase()
 }
